5e4b757b9c57f8073db2c61871bec3da8ef052ec
MisRay <mahdiahmadibdani@gmail.com>
2025-08-13 02:27:06 +0330

    add base template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 
-import './App.css';
+import "./App.css";
+import {Card,Box,Typography} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card sx={{
+      width: '100%',
+      height: '100vh',
+      borderRadius: '0',
+      backgroundColor: '#1C1F47',
+    }}>
+      <Box sx={{display:'row',alignItems:'center',justifyContent:'center',p:10}}>
+        <Box>
+          <Typography sx={{textAlign:'center',fontSize:75,fontFamily:'Monoton',color:'#fff'}}>Mahdi</Typography>
+        </Box>
+        <Box>
+          <Typography sx={{textAlign:'center',fontSize:75,fontFamily:'Monoton',color:'#fff'}}>Ahmadi</Typography>
+        </Box>
+      </Box >
+      <Box sx={{display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'#F1FAEE',m:5,p:2,borderRadius:'12px',gap:10,boxShadow:'10px 14px 1px rgba(0,0,0)'}}>
+          <Box sx={{
+            width:'230px',
+            height:'220px',
+            borderRadius:'22px',
+            backgroundColor:'#748CAB',
+          }}>
+
+          </Box>
+          <Box sx={{
+            width:'230px',
+            height:'220px',
+            borderRadius:'22px',
+            backgroundColor:'#748CAB',
+          }}>
+
+          </Box>
+          <Box sx={{
+            width:'230px',
+            height:'220px',
+            borderRadius:'22px',
+            backgroundColor:'#748CAB',
+          }}>
+
+          </Box>
+          <Box sx={{
+            width:'230px',
+            height:'220px',
+            borderRadius:'22px',
+            backgroundColor:'#748CAB',
+          }}>
+              
+          </Box>
+          <Box sx={{
+            width:'230px',
+            height:'220px',
+            borderRadius:'22px',
+            backgroundColor:'#748CAB',
+          }}>
+            
+          </Box>
+      </Box>
+    </Card>
   );
 }
 
