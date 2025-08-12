22aa81911145121b8ad67bb187e94b785a5e8638
MisRay <mahdiahmadibdani@gmail.com>
2025-08-13 02:44:59 +0330

    responsive mobile view(have bug)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import "./App.css";
 import {Card,Box,Typography} from "@mui/material";
 
 function App() {
@@ -10,20 +9,22 @@ function App() {
       borderRadius: '0',
       backgroundColor: '#1C1F47',
     }}>
-      <Box sx={{display:'row',alignItems:'center',justifyContent:'center',p:10}}>
+      <Box sx={{display:'row',alignItems:'center',justifyContent:'center',p:10,}}>
         <Box>
-          <Typography sx={{textAlign:'center',fontSize:75,fontFamily:'Monoton',color:'#fff'}}>Mahdi</Typography>
+          <Typography sx={{textAlign:'center',fontSize:{xs:55,sm:75},fontFamily:'Monoton',color:'#fff'}}>Mahdi</Typography>
         </Box>
         <Box>
-          <Typography sx={{textAlign:'center',fontSize:75,fontFamily:'Monoton',color:'#fff'}}>Ahmadi</Typography>
+          <Typography sx={{textAlign:'center',fontSize:{xs:55,sm:75},fontFamily:'Monoton',color:'#fff'}}>Ahmadi</Typography>
         </Box>
       </Box >
-      <Box sx={{display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'#F1FAEE',m:5,p:2,borderRadius:'12px',gap:10,boxShadow:'10px 14px 1px rgba(0,0,0)'}}>
+      <Box sx={{display:{xs:'row',sm:'flex'},overflowY:'auto',alignItems:'center',justifyContent:'center',backgroundColor:'#F1FAEE',m:5,p:2,borderRadius:'12px',gap:10,boxShadow:'10px 14px 1px rgba(0,0,0)'}}>
           <Box sx={{
             width:'230px',
             height:'220px',
             borderRadius:'22px',
             backgroundColor:'#748CAB',
+            m:{xs:3,sm:0},
+            p:{xs:3,sm:0}
           }}>
 
           </Box>
@@ -32,6 +33,8 @@ function App() {
             height:'220px',
             borderRadius:'22px',
             backgroundColor:'#748CAB',
+            m:{xs:3,sm:0},
+            p:{xs:3,sm:0}
           }}>
 
           </Box>
@@ -40,6 +43,8 @@ function App() {
             height:'220px',
             borderRadius:'22px',
             backgroundColor:'#748CAB',
+            m:{xs:3,sm:0},
+            p:{xs:3,sm:0}
           }}>
 
           </Box>
@@ -48,6 +53,8 @@ function App() {
             height:'220px',
             borderRadius:'22px',
             backgroundColor:'#748CAB',
+            m:{xs:3,sm:0},
+            p:{xs:3,sm:0}
           }}>
               
           </Box>
@@ -56,6 +63,8 @@ function App() {
             height:'220px',
             borderRadius:'22px',
             backgroundColor:'#748CAB',
+            m:{xs:3,sm:0},
+            p:{xs:3,sm:0}
           }}>
             
           </Box>
