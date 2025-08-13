68f6ee878cebd3698b3abd1243168ca17aab15b0
MisRay <mahdiahmadibdani@gmail.com>
2025-08-14 00:21:30 +0330

    test responsive mobile view

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       backgroundColor: '#1C1F47',
       overflow:'auto',
     }}>
-      <Box sx={{display:'row',alignItems:'center',justifyContent:'center',p:10,}}>
+      <Box sx={{display:'row',flexDirection:'column',alignItems:'center',justifyContent:'center',p:10,}}>
         <Box>
           <Typography sx={{textAlign:'center',fontSize:{xs:55,sm:75},fontFamily:'Monoton',color:'#fff'}}>Mahdi</Typography>
         </Box>
@@ -26,22 +26,26 @@ function App() {
           <Typography sx={{textAlign:'center',fontSize:{xs:55,sm:75},fontFamily:'Monoton',color:'#fff'}}>Ahmadi</Typography>
         </Box>
       </Box >
-      <Box sx={{display:{xs:'row',sm:'flex'},overflowY:'auto',alignItems:'center',justifyContent:'center',backgroundColor:'#F1FAEE',m:5,p:2,borderRadius:'12px',gap:10,boxShadow:'10px 14px 1px rgba(0,0,0)'}}>
+      <Box sx={{display:'flex',flexDirection:{xs:'column',sm:'row'},overflowY:'auto',alignItems:'center',justifyContent:'center',backgroundColor:'#F1FAEE',m:5,p:2,borderRadius:'12px',gap:10,boxShadow:'10px 14px 1px rgba(0,0,0)'}}>
           <Box sx={{
             display:'flex',
-            flexDirection:'row',
+            flexDirection:{xs:'column',sm:'column'},
+            minWidth:'130',
+            minHeight:'120',
             width:'230px',
             height:'220px',
             borderRadius:'22px',
             alignItems:'center',
             backgroundColor:'#748CAB',
             justifyContent:'center',
-            m:{xs:3,sm:0},
-            p:{xs:3,sm:0}
+            
           }}>
             <Fab sx={{width:'100px',height:'100px',borderRadius:'50%',backgroundImage:`url(${reactIcon})`,backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat',overflow:'none'}}>
               
             </Fab>
+            <Box>
+              <Typography>test</Typography>
+            </Box>
           </Box>
           <Box sx={{
             display:'flex',
@@ -52,8 +56,7 @@ function App() {
             alignItems:'center',
             backgroundColor:'#748CAB',
             justifyContent:'center',
-            m:{xs:3,sm:0},
-            p:{xs:3,sm:0}
+            
           }}>
             <Fab sx={{width:'100px',height:'100px',borderRadius:'50%',backgroundImage:`url(${GitIcon})`,backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat',overflow:'none'}}>
               
@@ -68,8 +71,6 @@ function App() {
             alignItems:'center',
             backgroundColor:'#748CAB',
             justifyContent:'center',
-            m:{xs:3,sm:0},
-            p:{xs:3,sm:0}
           }}>
             <Fab sx={{width:'100px',height:'100px'}}>
               
@@ -84,8 +85,6 @@ function App() {
             alignItems:'center',
             backgroundColor:'#748CAB',
             justifyContent:'center',
-            m:{xs:3,sm:0},
-            p:{xs:3,sm:0}
           }}>
             <Fab sx={{width:'100px',height:'100px'}}>
               
@@ -100,8 +99,6 @@ function App() {
             alignItems:'center',
             backgroundColor:'#748CAB',
             justifyContent:'center',
-            m:{xs:3,sm:0},
-            p:{xs:3,sm:0}
           }}>
             <Fab sx={{width:'100px',height:'100px'}}>
               
